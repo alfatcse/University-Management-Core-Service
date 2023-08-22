ae75eedf76d7289febb5c3c67f658e5ffc0f1912
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-08-22 21:58:15 +0200

    [Done] Prerequisite course included

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,22 +1,44 @@
 import prisma from '../../../shared/prisma';
 const insertIntoDb = async (data: any): Promise<any> => {
   const { preRequisiteCourses, ...courseData } = data;
-  const result = await prisma.course.create({
-    data: courseData,
-  });
-
-  if (preRequisiteCourses && preRequisiteCourses.length > 0) {
-    for (let index = 0; index < preRequisiteCourses.length; index++) {
-      const createPrerequisite = await prisma.courseToPrerequisite.create({
-        data: {
-          courseId: result.id,
-          preRequisiteId: preRequisiteCourses[index].courseId,
-        },
-      });
-      console.log(preRequisiteCourses[index].courseId, createPrerequisite);
+  const newCourse = await prisma.$transaction(async transactionClient => {
+    const result = await transactionClient.course.create({
+      data: courseData,
+    });
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      for (let index = 0; index < preRequisiteCourses.length; index++) {
+        const createPrerequisite =
+          await transactionClient.courseToPrerequisite.create({
+            data: {
+              courseId: result.id,
+              preRequisiteId: preRequisiteCourses[index].courseId,
+            },
+          });
+        console.log(preRequisiteCourses[index].courseId, createPrerequisite);
+      }
     }
+    return result;
+  });
+  if (newCourse) {
+    const responseData = await prisma.course.findUnique({
+      where: {
+        id: newCourse.id,
+      },
+      include: {
+        preRequisite: {
+          include: {
+            preRequisite: true,
+          },
+        },
+        preRequisiteFor: {
+          include: {
+            course: true,
+          },
+        },
+      },
+    });
+    return responseData;
   }
-  return result;
 };
 
 export const CourseService = { insertIntoDb };
