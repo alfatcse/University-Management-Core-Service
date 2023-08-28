839e1253727aef4c69fca842f1b465fea4d7ab15
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-08-29 01:46:16 +0200

    Offered Course Validation Error

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -1,9 +1,21 @@
 import { OfferedCourseSection } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 const insertIntoDB = async (
   data: OfferedCourseSection
 ): Promise<OfferedCourseSection> => {
-  console.log('dddd', data);
+  const isExistOfferedCourse = await prisma.offeredCourse.findFirst({
+    where: {
+      id: data.offeredCourseId,
+    },
+  });
+  if (!isExistOfferedCourse) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Offered Course Does not exist!'
+    );
+  }
   const result = await prisma.offeredCourseSection.create({ data });
   return result;
 };
