9eccf9d7c9d3c5c6deac669b579afefd194ae72d
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-08-29 01:34:48 +0200

    Zod validation added

--- src/app/modules/offeredCourseSection/offeredCourseSection.route.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.route.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { OfferedCourseSectionController } from './offeredCourseSection.controller';
 const router = express.Router();
-router.post('/', OfferedCourseSectionController.insertIntoDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  OfferedCourseSectionController.insertIntoDB
+);
 export const offeredCourseSectionRoutes = router;
