9d83e02a2a060fa6fcea1afa921f6a65ffdf0243
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-09-17 20:33:05 +0200

    Bug fixed

--- src/app/modules/academicSemester/academicSemester.constants.ts
+++ src/app/modules/academicSemester/academicSemester.constants.ts
@@ -10,3 +10,11 @@ export const AcademicSemesterFilterAbleFields = [
   'year',
   'endMonth',
 ];
+export const academicSemesterTitleCodeMapper: {
+  [key: string]: string;
+} = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+};
+export const EVENT_ACADEMIC_SEMESTER_CREATED = 'academic-semester.create';
